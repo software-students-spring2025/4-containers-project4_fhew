603789a4a50164d5462fd356040530e7854d991f
hhelenho <hth2016@stern.nyu.edu>
2025-04-04 14:58:13 -0400

    set up docker-compose.yml and env for running ML client in docker

---
+++ machine-learning-client/MLclient.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+# Connect to MongoDB service in Docker
+client = MongoClient("mongodb://mongodb:27017")
+db = client["sound_data"]
+collection = db["analysis"]
+
+def run_analysis():
+    sample_data = {
+        "audio1": "blue jay",
+        "audio2": "owl",
+    }
+    collection.insert_one(sample_data)
+    print("✔️ Data inserted into MongoDB!")
+
+if __name__ == "__main__":
+    run_analysis()
