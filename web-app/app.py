f5fe64105352ee119fe64baecf1f606e04e7f8f7
EmilyNey <emilymney@gmail.com>
2025-04-06 16:37:34 -0400

    Updated formatting for lint test

--- web-app/app.py
+++ web-app/app.py
@@ -1,26 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
-import pymongo
+"""Flask web app to locate nearest emergency services for users."""
+
+from flask import Flask, render_template, request
 from pymongo import MongoClient
-import os
 
 app = Flask(__name__)
 client = MongoClient("mongodb://mongodb:27017")
-db = client["emergency_services"] 
+db = client["emergency_services"]
 #we will obv have to change this once the mongodb database is sest up
 
 @app.route("/")
 def home():
+    """Render the homepage."""
     return render_template("home.html")
 
 @app.route("/find-location", methods=["POST"])
 def find_location():
-   data = request.get_json()
-   db.locations.insert_one(data)
-   return {"message": "Location found"}
+    """Collect geolocationdata and store it in the mongodb database."""
+    data = request.get_json()
+    db.locations.insert_one(data)
+    return {"message": "Location found"}
 
 @app.route("/show-results")
 def show_results():
-    nearby_services = list(db.results.find()) 
+    """Show results from the nearest emergency services search."""
+    nearby_services = list(db.results.find())
     #temp because I'm not sure how this will work with the ML model
     return render_template("show_results.html", services=nearby_services)
 
