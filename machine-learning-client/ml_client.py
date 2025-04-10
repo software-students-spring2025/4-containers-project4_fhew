9d49308383ee6b563d52b92b90a07b57c96cd2b4
Wyatt-Destabelle <106564974+Wyatt-Destabelle@users.noreply.github.com>
2025-04-10 01:03:00 -0400

    Added database commands to ML client, and a UTM to LatLong converter

--- machine-learning-client/ml_client.py
+++ machine-learning-client/ml_client.py
@@ -14,14 +14,15 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 import requests
 from config import GOOGLE_MAPS_API_KEY
+import utm
 
 app = Flask(__name__)
 
 # Connect to MongoDB service in Docker
 client = MongoClient("mongodb://mongodb:27017")
-db = client["gps_data"]
-raw_data_collection = db["gps_input"]  # DB of user input
-analysis_collection = db["analysis"]  # DB of stored analysis
+db = client["emergency_services"]
+reqDB = db["Request"]  # DB of user input
+resDB = db["Result"]  # DB of stored analysis
 
 
 # Import CSV
@@ -30,33 +31,51 @@ RISK_THRESHOLDS_KM = [1, 5, 10]  # these are cutoffs for risks
 
 
 # analysis is invoked by the interface of the web app part
-def run_analysis(user_location):
+def run_analysis(reqID):
+    
     """Main function for ML analysis."""
+    req = reqDB.find_one({"_id":reqID})
     stations = load_station_data(CSV_PATH)
     nearby_stations = find_near_by_stations(
-        user_location["latitude"], user_location["longitude"], stations
+        req.get("location")["latitude"], req.get("location")["longitude"], stations
     )
-    risk_level = assign_risk_level(nearby_stations)
-    analysis_result = {
-        "user_location": user_location,
-        "nearby_stations": nearby_stations,
-        "risk_level": risk_level,
-    }
-    analysis_collection.insert_one(analysis_result.copy())
+    IDArray = []
+    for station in nearby_stations:
+        risk_level = assign_risk_level(nearby_stations)
+        res = {
+            "name" : station["Station Name"],
+            "location" : {"longitude": station["longitude"], "latitude" : station["latitude"]},
+            "address" : station["Address"],
+            "travel_distance" : station["distance_km"] 
+            #travel time
+            #urgency
+        }
+        retID = resDB.insert_one(res).inserted_id
+        IDArray.append(retID)
+
+    reqDB.update_one({"_id":reqID},{"$set" : {"resultIDs":IDArray}})
     print("Analysis complete. Data inserted into MongoDB!")
-    return analysis_result
+    return reqID
+
+
+def UTMtoLatLong(easting,northing):
+    Lat, Long = utm.to_latlon(easting,northing,18,'T')
+    return Lat,Long
 
 
 def find_near_by_stations(user_lat, user_lon, stations, radius_km=5):
     """Find the nearest fire station."""
     nearby = []
     for station in stations:
+        stationLat,stationLong = UTMtoLatLong(station["x"],station["y"])
         distance = haversine_distance(
-            user_lat, user_lon, station["latitude"], station["longitude"]
+            user_lat, user_lon, stationLat, stationLong
         )
         if distance <= radius_km:
             station_data = station.copy()
             station_data["distance_km"] = distance
+            station_data["longitude"] = stationLong
+            station_data["latitude"] = stationLat
             nearby.append(station_data)
     # Sort by distance, closest first.
     return sorted(nearby, key=lambda s: s["distance_km"])
